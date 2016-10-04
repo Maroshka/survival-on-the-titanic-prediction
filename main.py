f0b6889006ef35c3c5a1b74f975eef7162ca9a3c
maroshka <maroshka202@gmail.com>
2016-10-04 22:14:49 +0300

    os-2016-4

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import statsmodels.discrete.discrete_model as st
 from patsy import *
 import pylab as plt 
 import statsmodels.api as st
+from sklearn.metrics import classification_report
 
 """Data Preperation"""
 df = pd.read_csv('training.csv')
@@ -19,11 +20,13 @@ dftrain = df[:600]
 dftest = df[600:]
 
 formula = 'Survived ~ C(Pclass) + C(Sex) + Age + SibSp'
+f2 = 'Survived ~ C(Pclass) + C(Sex) + Age + SibSp'
 
 #y_train, x_train = dftrain['Survived'], dftrain[['Age', 'Sex', 'Parch', 'SibSp', 'Pclass', 'Embarked']]
 #y_test, x_test = dftest['Survived'], dftest[['Age', 'Sex', 'Parch', 'SibSp', 'Pclass', 'Embarked']]
 
 y_train,x_train = dmatrices(formula, data=dftrain,return_type='dataframe')
+y_test, x_test = dmatrices(f2, data=dftest,return_type='dataframe')
 
 model = st.Logit(y_train, x_train)
 res = model.fit()
@@ -33,7 +36,7 @@ print res.summary() #useful to check if the predictors are significant or not, s
 """Distribution of predictions"""
 kde_res =  st.nonparametric.KDEUnivariate(res.predict())
 kde_res.fit()
-plt.subplot(kde_res.support, kde_res.density)
+plt.plot(kde_res.support, kde_res.density)
 plt.fill_between(kde_res.support, kde_res.density, alpha = 0.5)
 plt.title("Distribution of the predictions")
 
@@ -57,6 +60,15 @@ plt.grid(True, linewidth=0.15)
 plt.title("The Change of Survival Probability by Age")
 plt.xlabel("Predicted chance of survival")
 plt.ylabel("Age")
+
+"""Evaluation based on test data """
+
+y_pred = res.predict(x_test)
+y_pred_flag = y_pred > 0.7
+
+print pd.crosstab(y_test.Survived, y_pred_flag,rownames = ['Actual'],colnames = ['Predicted'])
+print classification_report(y_test, y_pred_flag)
+
 
 
 
