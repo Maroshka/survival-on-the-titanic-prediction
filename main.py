a0dc0be8fa26adfb5be17036827e25553b0516a6
maroshka <maroshka202@gmail.com>
2016-10-04 09:25:45 +0300

    2016-10-4

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import clean as cl 
 import statsmodels.discrete.discrete_model as st 
 from patsy import *
+import pylab as plt 
+import statsmodels.api as st
 
 """Data Preperation"""
 df = pd.read_csv('training.csv')
@@ -25,8 +27,36 @@ y_train,x_train = dmatrices(formula, data=dftrain,return_type='dataframe')
 
 model = st.Logit(y_train, x_train)
 res = model.fit()
-print res.summary()
+print res.summary() #useful to check if the predictors are significant or not, since p=val <0.05 for the chosen columns, it means they're all significant predictors
 
+"""Model Evaluation using the training dataset"""
+"""Distribution of predictions"""
+kde_res =  st.nonparametric.KDEUnivariate(res.predict())
+kde_res.fit()
+# plt.subplot(kde_res.support, kde_res.density)
+# plt.fill_between(kde_res.support, kde_res.density, alpha = 0.5)
+# plt.title("Distribution of the predictions")
+
+"""Change of survival probability by gender"""
+plt.scatter(res.predict(), x_train['C(Sex)[T.male]'], alpha=0.5)
+plt.grid(b=True, which='major', axis='x')
+plt.xlabel('Predicted chance of survival')
+plt.ylabel('Gender=> 1:male, 0:female')
+plt.title('The change of survival probability by gender')
+
+"""The Change of Survival Probability by class"""
+plt.scatter(res.predict(),x_train['C(Pclass)[T.3]'] , alpha=0.2)
+plt.xlabel("Predicted chance of survival")
+plt.ylabel("Class Bool") # Boolean class to show if its 3rd class
+plt.grid(b=True, which='major', axis='x')
+plt.title("The Change of Survival Probability by class")
+
+"""Change of survival probability by age"""
+plt.scatter(res.predict(),x_train.Age , alpha=0.2)
+plt.grid(True, linewidth=0.15)
+plt.title("The Change of Survival Probability by Age")
+plt.xlabel("Predicted chance of survival")
+plt.ylabel("Age")
 
 
 
